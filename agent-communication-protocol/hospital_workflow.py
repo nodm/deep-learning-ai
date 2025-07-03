7ca0ae5b9cbc24a82beba6128edd25ff8b399824
Dmytro Novikov <nodm.pe@gmail.com>
2025-07-03 08:41:14 +0300

    chore: add the course from a separate repo

---
+++ agent-communication-protocol/hospital_workflow.py
@@ -0,0 +1,22 @@
+import nest_asyncio
+nest_asyncio.apply()
+
+from acp_sdk.client import Client
+import asyncio
+from colorama import Fore 
+
+async def run_hospital_workflow() -> None:
+    async with Client(base_url="http://localhost:8001") as insurer, Client(base_url="http://localhost:8000") as hospital:
+        run1 = await hospital.run_sync(
+            agent="health_agent", input="Do I need rehabilitation after a shoulder reconstruction?"
+        )
+        content = run1.output[0].parts[0].content
+        print(Fore.LIGHTMAGENTA_EX+ content + Fore.RESET)
+
+        run2 = await insurer.run_sync(
+            agent="policy_agent", input=f"Context: {content} What is the waiting period for rehabilitation?"
+        )
+        print(Fore.YELLOW + run2.output[0].parts[0].content + Fore.RESET)
+
+if __name__ == "__main__":
+    asyncio.run(run_hospital_workflow())
---
+++ agent-communication-protocol/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from agent-communication-protocol!")
+
+
+if __name__ == "__main__":
+    main()
